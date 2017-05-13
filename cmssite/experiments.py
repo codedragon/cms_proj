6b93c452d0365a8e114e2626614209d9bc1aa026
pvosper <pvosper@mac.com>
2017-05-13 07:33:31 -0700

    Some progress but mostly failed attempts

--- cmssite/experiments.py
+++ cmssite/experiments.py
@@ -11,9 +11,17 @@ def main():
     for entry in poa:
         print(entry.pk, entry.title)
         # Only with a specific entry object can we find many to many data
+        c_count = 0
         cats = entry.categories.all()
         for cat in cats:
-            print('\t', cat)
+            c_count += 1
+            print('Category: %s %s' % (c_count, cat))
+
+
+colors = ['red', 'yellow', 'blue', 'green']
+for color in colors:
+    print('color: %s' % color)
+
 
 
 if __name__ == "__main__":
