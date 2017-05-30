eadc59f27026d9b8a35d29992d5c7df3552087f1
pvosper <pvosper@mac.com>
2017-05-30 06:16:23 -0700

    Trouble with vendor ForeignKey vendor_id

--- cmssite/cmsblog/views.py
+++ cmssite/cmsblog/views.py
@@ -156,11 +156,8 @@ def event_detail(request, event_id):
     except Event.DoesNotExist:
         raise Http404
     venues = Venue.objects.all()
-    event.venue = venues[event.venue_id]
+    event.venue = venues[0]  # [event.venue_id]
     context = {'event': event}
-    logger.info('request: %s', request)
-    logger.info('event: %s', event)  # returns name of event
-    logger.info('type event: %s', type(event))
     return render(request, 'event_detail.html', context)
 
 
